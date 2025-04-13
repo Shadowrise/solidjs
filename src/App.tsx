a3d871dd47e9f95efad60267ce1ee510734cfb71
Vyacheslav Emtsov <vyacheslav.emtsov@fastdev.se>
2025-04-13 21:47:58 +0300

    + api

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
-import type { Component } from 'solid-js';
+import {Component, createResource, createSignal, For} from 'solid-js';
 
 import logo from './logo.svg';
 import styles from './App.module.css';
 
+const fetchUrl = 'https://solidjs-backend-fmgvh0adhtfgfyh4.westeurope-01.azurewebsites.net';
+
+type WeatherForecast = {
+  date: string;         
+  temperatureC: number; 
+  summary: string;      
+  temperatureF: number; 
+};
+
+const fetchWeatherForecast = async () => {
+  const response = await fetch(`${fetchUrl}/weatherForecast`);
+  const result = await response.json();
+  return result as WeatherForecast[];
+}
+
 const App: Component = () => {
+  const [weatherForecast] = createResource(fetchWeatherForecast);
+  
   return (
     <div class={styles.App}>
       <header class={styles.header}>
@@ -11,14 +28,17 @@ const App: Component = () => {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
+        {weatherForecast.loading && <p>Loading...</p>}
+        {weatherForecast.error && <p>Error: {weatherForecast.error.message}</p>}
+        <ul>
+          <For each={weatherForecast()}>
+            {(forecast) => (
+              <li style={{margin: 0, padding: 0}}>
+                <span>{forecast.date} {forecast.temperatureC}°C, {forecast.summary}</span>
+              </li>
+            )}
+          </For>
+        </ul>
       </header>
     </div>
   );
